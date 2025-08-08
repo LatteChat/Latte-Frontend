fd3dfa603aab8c4381a62eac3fd3ed662491acf0
KimYuKyung <kyg0954@naver.com>
2025-08-08 16:12:58 +0900

    test

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-
 export default function NamePage() {
-  const [name, setName] = useState<any>()
   return (
     <div>
       <p className="bg-slate-300 text-red-500">안녕</p>
