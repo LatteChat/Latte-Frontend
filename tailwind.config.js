0efdd370e8826c3a2843b153a7f5201aaf874785
KimYuKyung <kyg0954@naver.com>
2025-08-10 22:52:58 +0900

    chore: tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/features/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
     './src/app/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        main: 'calc(100svh - 5rem)',
+      },
+      fontFamily: {
+        apple: ['AppleSDGothicNeoB00', 'Noto Sans KR', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
