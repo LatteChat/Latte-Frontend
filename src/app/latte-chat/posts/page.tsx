9c3bf87bfc0ab89e05ccf309eb53b85211657404
KimYuKyung <kyg0954@naver.com>
2025-08-12 00:19:34 +0900

    feat: 라떼챗 커뮤니티 페이지 UI 제작

---
+++ src/app/latte-chat/posts/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import NavTabBar from '@/shared/components/NavTabBar'
+import Topbar from '@/shared/components/Topbar'
+import PostListContainer from '@/shared/containers/PostListContainer'
+
+const TOPBAR_ICONS = [
+  {
+    iconUrl: '/icons/alarm-icon.svg',
+    alt: '알람',
+    href: '',
+  },
+  {
+    iconUrl: '/icons/search-icon.svg',
+    alt: '검색',
+    href: '',
+  },
+]
+
+export default function LatteChatCommunityPage() {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 pt-3">
+        <Topbar icons={TOPBAR_ICONS} />
+        <NavTabBar />
+      </div>
+
+      <main className="min-h-main flex h-auto flex-col bg-gray-100 py-5">
+        <PostListContainer />
+      </main>
+    </div>
+  )
+}
