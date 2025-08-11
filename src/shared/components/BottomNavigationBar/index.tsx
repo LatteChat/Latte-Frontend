991c233505cc5e0973b5a6c619a811c8f91b3d50
KimYuKyung <kyg0954@naver.com>
2025-08-12 02:31:29 +0900

    feat: BottomNavigationBar에 링크 걸고 아이콘 변경

--- src/shared/components/BottomNavigationBar/index.tsx
+++ src/shared/components/BottomNavigationBar/index.tsx
@@ -1,43 +1,89 @@
+'use client'
+
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 const NAVIGATION_OPTIONS = [
   {
-    icon: '/icons/daum-icon.svg',
+    iconUrl: '/icons/daum-icon.svg',
     value: 'home',
+    name: '홈',
+    href: '/',
   },
   {
-    icon: '/icons/daum-icon.svg',
-    value: 'prev',
+    iconUrl: '/icons/back-arrow-icon.svg',
+    value: 'back',
+    name: '이전',
   },
   {
-    icon: '/icons/daum-icon.svg',
+    iconUrl: '/icons/next-arrow-icon.svg',
     value: 'next',
+    name: '다음',
   },
   {
-    icon: '/icons/daum-icon.svg',
+    iconUrl: '/icons/search-icon.svg',
     value: 'search',
+    name: '검색',
+    href: '',
   },
   {
-    icon: '/icons/daum-icon.svg',
-    value: 'what',
+    iconUrl: '/icons/share-icon.svg',
+    value: 'share',
+    name: '공유',
+    href: '',
   },
   {
-    icon: '/icons/daum-icon.svg',
+    iconUrl: '/icons/more-icon.svg',
     value: 'more',
+    name: '더보기',
+    href: '',
   },
 ]
 
 export default function BottomNavigationBar() {
+  const router = useRouter()
+
+  const handleClick = (value: string) => {
+    if (value === 'back') router.back()
+    else if (value === 'next') router.forward()
+  }
+
   return (
-    <section className="flex h-20 w-full">
-      {NAVIGATION_OPTIONS.map((option) => {
-        return (
-          <button
-            key={option.value}
-            className="flex flex-1 items-start justify-center p-4"
-          >
-            <img src={option.icon} className="aspect-square w-5 min-w-2" />
-          </button>
-        )
-      })}
-    </section>
+    <nav className="h-20 w-full">
+      <ul className="flex h-full w-full">
+        {NAVIGATION_OPTIONS.map(({ iconUrl, value, name, href }) => {
+          const content = (
+            <img
+              src={iconUrl}
+              alt={`${name} 아이콘`}
+              className="aspect-square h-6 w-6 min-w-2"
+            />
+          )
+
+          return (
+            <li key={value} className="flex-1">
+              {value === 'back' || value === 'next' ? (
+                <button
+                  onClick={() => handleClick(value)}
+                  className="flex h-full w-full justify-center p-4"
+                  aria-label={name}
+                  type="button"
+                >
+                  {content}
+                </button>
+              ) : (
+                <Link
+                  href={href || ''}
+                  className="flex h-full w-full justify-center p-4"
+                  aria-label={name}
+                >
+                  {content}
+                </Link>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   )
 }
