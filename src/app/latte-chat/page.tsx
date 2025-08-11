f64ce895087b4206422c1749b4ffa302da1f8359
KimYuKyung <kyg0954@naver.com>
2025-08-12 00:20:39 +0900

    feat: 라떼챗 홈 페이지에 NavTabBar 컴포넌트 추가

--- src/app/latte-chat/page.tsx
+++ src/app/latte-chat/page.tsx
@@ -2,6 +2,7 @@
 
 import BaristaRankContainer from '@/features/home/containers/BaristaRankContainer'
 import PopularPostListContainer from '@/features/home/containers/PopularPostListContainer'
+import NavTabBar from '@/shared/components/NavTabBar'
 import Topbar from '@/shared/components/Topbar'
 import PostListContainer from '@/shared/containers/PostListContainer'
 
@@ -21,7 +22,10 @@ const TOPBAR_ICONS = [
 export default function LatteChatHomePage() {
   return (
     <div>
-      <Topbar icons={TOPBAR_ICONS} />
+      <div className="flex flex-col gap-4 pt-3">
+        <Topbar icons={TOPBAR_ICONS} />
+        <NavTabBar />
+      </div>
 
       <main className="min-h-main flex h-auto flex-col bg-gray-100 py-5">
         <div className="mb-4">
