7e3b304a21add94f5b90cff1537ef6fb98bc296e
KimYuKyung <kyg0954@naver.com>
2025-08-11 20:33:09 +0900

    chore: svgr 설치

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test?: RegExp | ((str: string) => boolean) }) =>
+        rule.test instanceof RegExp && rule.test.test('.svg')
+    )
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 export default nextConfig
