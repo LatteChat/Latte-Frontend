6935ed3591c06d43c9bca3b34c6688a717fa564f
KimYuKyung <kyg0954@naver.com>
2025-08-16 18:03:49 +0900

    feat: PostCard를 커뮤니티와 글 보관함 페이지에서 사용하기 위해 props 변경
    - props로 shadow, status, meta show 여부 추가

--- src/shared/components/PostCard/index.tsx
+++ src/shared/components/PostCard/index.tsx
@@ -1,42 +1,52 @@
 import Image from 'next/image'
 import PostTag from '../PostTag'
 
+type LetterStatus = 'ANSWERED' | 'ACCEPTED' | 'SAVED' | 'PENDING'
+
+const LETTER_STATUS_LABEL: Record<LetterStatus, string> = {
+  ANSWERED: '답변 완료',
+  ACCEPTED: '채택 완료',
+  SAVED: '저장됨',
+  PENDING: '답변 대기 중',
+}
+
+type Post = {
+  tag: string
+  title: string
+  content: string
+  date: string
+  likeCount: number
+  commentCount: number
+  status?: LetterStatus
+}
+
 type PostCardProps = {
-  post: {
-    tags?: string[]
-    title: string
-    content: string
-    date: string
-    likeCount: number
-    commentCount: number
-  }
+  post: Post
+  showStatus?: boolean
+  showShadow?: boolean
+  showMeta?: boolean
 }
 
 export default function PostCard({
-  post: { tags = [], title, content, date, likeCount, commentCount },
+  post: { tag, title, content, date, likeCount, commentCount, status },
+  showStatus,
+  showShadow,
+  showMeta,
 }: PostCardProps) {
-  const displayTags = tags.length > 3 ? tags.slice(0, 3) : tags
-
   return (
-    <div className="flex w-full cursor-pointer flex-col items-center gap-4 rounded-xl bg-white p-5">
+    <article
+      className={`${showShadow ? 'shadow' : ''} relative flex w-full cursor-pointer flex-col items-center gap-4 rounded-xl bg-white p-5`}
+    >
+      {status && showStatus && (
+        <span className="b6 absolute -top-4 left-4 rounded-full bg-gray-300 px-2 py-1">
+          {LETTER_STATUS_LABEL[status]}
+        </span>
+      )}
+
       <div className="flex w-full gap-5">
         <div className="flex w-full flex-col gap-2">
           <div className="flex gap-2">
-            {displayTags.map((tag) => {
-              return <PostTag key={tag} tag={tag} />
-            })}
-            {tags.length > 3 && (
-              <span className="b9 flex select-none items-center justify-center gap-1 rounded-[0.25rem] bg-gray-200 px-[0.28rem] py-[0.125rem] text-gray-400">
-                {new Array(3).fill(0).map((_, index) => {
-                  return (
-                    <span
-                      key={index}
-                      className="inline-block h-[0.125rem] w-[0.125rem] bg-gray-400"
-                    ></span>
-                  )
-                })}
-              </span>
-            )}
+            <PostTag tag={tag} />
           </div>
 
           <div className="flex w-full flex-col gap-1">
@@ -58,25 +68,27 @@ export default function PostCard({
 
       <div className="flex w-full items-center justify-between gap-2 text-gray-400">
         <span className="b9">{date}</span>
-        <div className="flex gap-2">
-          <div className="flex items-center gap-[1px]">
-            <img
-              src="/icons/empty-heart-icon.svg"
-              className="aspect-square h-[0.875rem] w-[0.875rem]"
-              alt="좋아요 아이콘"
-            />
-            <span className="b9">{likeCount}</span>
+        {showMeta && (
+          <div className="flex gap-2">
+            <div className="flex items-center gap-[1px]">
+              <img
+                src="/icons/empty-heart-icon.svg"
+                className="aspect-square h-[0.875rem] w-[0.875rem]"
+                alt="좋아요 아이콘"
+              />
+              <span className="b9">{likeCount}</span>
+            </div>
+            <div className="flex items-center gap-[1px]">
+              <img
+                src="/icons/comment-icon.svg"
+                className="aspect-square h-3 w-3"
+                alt="댓글 아이콘"
+              />
+              <span className="b9">{commentCount}</span>
+            </div>
           </div>
-          <div className="flex items-center gap-[1px]">
-            <img
-              src="/icons/comment-icon.svg"
-              className="aspect-square h-3 w-3"
-              alt="댓글 아이콘"
-            />
-            <span className="b9">{commentCount}</span>
-          </div>
-        </div>
+        )}
       </div>
-    </div>
+    </article>
   )
 }
