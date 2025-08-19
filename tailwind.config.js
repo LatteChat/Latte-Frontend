b10d53b6d4d739f7e77d8cb203a70dacdb634a5e
KimYuKyung <kyg0954@naver.com>
2025-08-19 23:19:49 +0900

    style: tailwind color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,41 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#E5CBA5',
+        secondary: {
+          brown: {
+            1: '#FFF9EE',
+            2: '#C9A070',
+            3: '#A57939',
+            4: '#6E4F36',
+            5: '#2E1C1B',
+          },
+          red: '#FB3636',
+          chat: '#FB6029',
+        },
+        gray: {
+          1: '#F3F3F3',
+          2: '#E7E7E7',
+          3: '#D9D9D9',
+          4: '#B5B5B5',
+          5: '#989898',
+          6: '#7C7C7C',
+          7: '#484848',
+        },
+        interaction: {
+          error: '#DF1D1D',
+          success: '#0073CB',
+        },
+        backgroundImage: {
+          'latte-gradient-1':
+            'linear-gradient(180deg, #FFFFFF 0%, #E5CBA5 100%)',
+          'latte-gradient-2':
+            'linear-gradient(90deg, #FFFFFF 0%, #E5CBA5 100%)',
+          'latte-gradient-3':
+            'linear-gradient(180deg, #C494A1 0%, #D4CD48 50%, #D4CD48 100%)',
+        },
+      },
       minHeight: {
         main: 'calc(100svh - 5rem)',
       },
@@ -16,6 +51,9 @@ module.exports = {
       },
       borderRadius: {
         10: '0.625rem',
+      },
+      boxShadow: {
+        'select-button': '0 0 4px 0 rgba(0, 0, 0, 0.20)',
       },
     },
   },
