3c82d0f2a81c043a1bd7339fdfcf517006019307
KimYuKyung <kyg0954@naver.com>
2025-08-28 19:26:38 +0900

    feat: 사연 좋아요 기능 구현

--- src/features/letter/services/letterService.client.ts
+++ src/features/letter/services/letterService.client.ts
@@ -23,3 +23,27 @@ export const saveLetter = async (
     body: JSON.stringify(body),
   })
 }
+
+// 사연 좋아요
+export const savePostLike = async ({
+  letterId,
+  userId,
+  memberType,
+}: {
+  letterId: number
+  userId: number
+  memberType: string // SENIOR, JUNIOR
+}) => {
+  const token = localStorage.getItem('accessToken')
+  if (!token) throw new Error('토큰이 없습니다.')
+
+  return await httpCSR(
+    `/letter/${letterId}/${userId}/heart?memberType=${memberType}`,
+    {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+}
