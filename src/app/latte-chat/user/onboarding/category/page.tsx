d371cfa0b0469de5a5c3de3838ef66e65a02e66e
KimYuKyung <kyg0954@naver.com>
2025-08-29 11:15:49 +0900

    refactor: 유저 정보 수집 카테고리 페이지 인증 여부 제거

--- src/app/latte-chat/user/onboarding/category/page.tsx
+++ src/app/latte-chat/user/onboarding/category/page.tsx
@@ -6,35 +6,32 @@ import StepTitle from '@/features/user/onboarding/components/StepTitle'
 import { useSignupStore } from '@/features/user/stores/signupStore'
 import { CATEGORIES } from '@/shared/types/Type'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function UserOnBoardingCategoryPage() {
   const router = useRouter()
-  const memberType = useSignupStore((state) => state.memberType)
   const categoryList = useSignupStore((state) => state.categoryList)
   const setCategoryList = useSignupStore((state) => state.setCategoryList)
+  const [isAvailable, setIsAvailable] = useState(true)
 
   const handleClickNextButton = () => {
     if (categoryList.length <= 0) {
-      alert('카테고리를 한 개 이상 선택해주세요')
+      setIsAvailable(false)
       return
     }
 
-    const hasAuthenticatedCategory = categoryList.some(
-      (category) => category === 'CAREER' || category === 'EDUCATION'
-    )
-
-    if (hasAuthenticatedCategory && memberType === 'SENIOR') {
-      router.push(`/latte-chat/user/onboarding/experience`)
-    } else {
-      router.push(`/latte-chat/user/onboarding/agreements`)
-    }
+    router.push(`/latte-chat/user/onboarding/introduce`)
   }
 
   return (
-    <main className="relative flex h-auto min-h-main flex-1 flex-col space-y-14 bg-white px-5 py-10 pb-32">
-      <StepTitle title={`관심 있는 카테고리를 선택해주세요.`} activeIndex={2} />
+    <main className="relative flex h-auto min-h-main flex-1 flex-col space-y-8 bg-white px-5 py-10 pb-32">
+      <StepTitle
+        title={`멘토링 주제를 골라주세요`}
+        intro="본인이 자신 있게 답할 수 있는 주제를 골라주세요."
+        activeIndex={3}
+      />
 
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-1.5">
         <div className="flex flex-wrap gap-3">
           {CATEGORIES.map((category) => {
             const isSelected = categoryList.some(
@@ -52,10 +49,8 @@ export default function UserOnBoardingCategoryPage() {
             )
           })}
         </div>
-        {memberType === 'SENIOR' && (
-          <p className="b6 text-gray-5">
-            * 인증 마크가 붙은 건 간단한 인증이 필요합니다.
-          </p>
+        {!isAvailable && (
+          <span className="b9 text-secondary-red">* 선택이 필요합니다</span>
         )}
       </div>
 
