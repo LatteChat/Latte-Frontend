e9ca99a4c5355c855e5813ce68911424f06ab89f
KimYuKyung <kyg0954@naver.com>
2025-09-02 17:13:02 +0900

    feat: 사연 삭제 커스텀훅 구현

--- src/features/letter/services/letterService.client.ts
+++ src/features/letter/services/letterService.client.ts
@@ -248,3 +248,16 @@ export const fetchLetterArchiveNewState = async ({
     },
   })
 }
+
+// 사연 삭제
+export const deleteLetter = async ({ letterId }: { letterId: number }) => {
+  const token = localStorage.getItem('accessToken')
+  if (!token) throw new Error('토큰이 없습니다.')
+
+  return await httpCSR(`/junior/${letterId}/delete`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+}
