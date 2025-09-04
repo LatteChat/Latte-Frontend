999b1a9895046fca6dbecafc186e46a79299443b
KimYuKyung <kyg0954@naver.com>
2025-09-04 11:34:59 +0900

    feat: pwa 설정

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['d2e9ojs4st7ryw.cloudfront.net'],
   },
