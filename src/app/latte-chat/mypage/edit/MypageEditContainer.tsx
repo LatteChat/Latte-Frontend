1937b7a0158fc672736fc731059d1ffe86e8d2ec
KimYuKyung <kyg0954@naver.com>
2025-09-05 12:10:30 +0900

    fix: 모바일 태그 추가 오류 해결

--- src/app/latte-chat/mypage/edit/MypageEditContainer.tsx
+++ src/app/latte-chat/mypage/edit/MypageEditContainer.tsx
@@ -8,9 +8,18 @@ import { useEffect, useState } from 'react'
 export default function MypageEditContainer() {
   const { data: userInfo } = useGetMyInfoQuery()
 
+  console.log(userInfo)
   const [selectedTags, setSelectedTags] = useState<string[]>(
     userInfo?.tag ?? []
   )
+
+  useEffect(() => {
+    if (!userInfo) return
+    setSelectedTags(userInfo.tag ?? [])
+    setNickname(userInfo.name ?? '')
+    setIntroduce(userInfo.introduce ?? '')
+  }, [userInfo])
+
   const [nickname, setNickname] = useState('')
   const [searchTag, setSearchTag] = useState('')
   const [introduce, setIntroduce] = useState('')
@@ -21,12 +30,7 @@ export default function MypageEditContainer() {
 
   const { data: searchedTags } = useGetTagListQuery({ keyword: searchTag })
 
-  useEffect(() => {
-    if (!userInfo) return
-    setSelectedTags(userInfo.tag ?? [])
-    setNickname(userInfo.name ?? '')
-    setIntroduce(userInfo.introduce ?? '')
-  }, [userInfo])
+  console.log(searchedTags)
 
   const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value)
@@ -40,15 +44,19 @@ export default function MypageEditContainer() {
     setIntroduce(e.target.value)
   }
 
-  const handleAddTag = () => {
-    const newTag = searchTag.trim()
-    if (!newTag) return
-    if (selectedTags.includes(newTag)) return
-    if (selectedTags.length >= 3) {
-      return alert('태그는 최대 3개까지 추가할 수 있습니다.')
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      const newTag = searchTag.trim()
+
+      if (!newTag) return
+      if (selectedTags.includes(newTag)) return
+      if (selectedTags.length >= 3)
+        return alert('태그는 최대 3개까지 추가할 수 있습니다.')
+
+      setSelectedTags((prev) => [...prev, newTag])
+      setSearchTag('')
     }
-    setSelectedTags((prev) => [...prev, newTag])
-    setSearchTag('')
   }
 
   const handleRemoveTag = (tagToRemove: string) => {
@@ -59,6 +67,7 @@ export default function MypageEditContainer() {
     e.preventDefault()
 
     const formData = new FormData()
+
     selectedTags.forEach((tag) => formData.append('tag', tag))
     formData.append('introduce', introduce)
     formData.append('name', nickname)
@@ -69,10 +78,18 @@ export default function MypageEditContainer() {
 
     if (userInfo.type === 'SENIOR') {
       if (!userInfo?.seniorId) return
-      updateSeniorUserMutate({ seniorId: userInfo.seniorId, payload: formData })
+
+      updateSeniorUserMutate({
+        seniorId: userInfo?.seniorId,
+        payload: formData,
+      })
     } else {
       if (!userInfo?.juniorId) return
-      updateJuniorUserMutate({ juniorId: userInfo.juniorId, payload: formData })
+
+      updateJuniorUserMutate({
+        juniorId: userInfo?.juniorId,
+        payload: formData,
+      })
     }
   }
 
@@ -82,7 +99,6 @@ export default function MypageEditContainer() {
         <h1 className="b2">프로필 수정</h1>
       </div>
 
-      {/* 메인 저장 폼 */}
       <form
         onSubmit={handleSave}
         className="relative flex h-full flex-col px-5 py-7"
@@ -112,7 +128,6 @@ export default function MypageEditContainer() {
               <span className="text-gray-5">{nickname.length}/10</span>
             </div>
           </div>
-
           <div className="flex flex-wrap gap-3">
             {selectedTags.map((tag) => (
               <span
@@ -132,49 +147,60 @@ export default function MypageEditContainer() {
         </div>
 
         <div className="relative mb-10 mt-4">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault()
-              handleAddTag()
-            }}
-            className="flex gap-2 rounded-10 bg-gray-1 px-2"
-          >
+          <div className="flex items-center gap-2 rounded-10 bg-gray-1 px-2">
             <input
               placeholder="#검색하거나 직접 태그 추가하기"
               className="b12 flex-1 bg-transparent py-2.5 outline-none placeholder:text-gray-5"
               onChange={handleChangeTag}
+              onKeyDown={handleKeyDown}
               value={searchTag}
-              enterKeyHint="done"
             />
-            <img src="/icons/search-icon.svg" />
-          </form>
+            <button
+              type="button"
+              className="b12 rounded bg-secondary-brown-2 px-1 py-0.5 text-white"
+              onClick={() => {
+                const newTag = searchTag.trim()
+
+                if (!newTag) return
+                if (selectedTags.includes(newTag)) return
+                if (selectedTags.length >= 3)
+                  return alert('태그는 최대 3개까지 추가할 수 있습니다.')
+
+                setSelectedTags((prev) => [...prev, newTag])
+                setSearchTag('')
+              }}
+            >
+              추가
+            </button>
+          </div>
 
           {searchedTags?.length > 0 && (
             <ul className="absolute top-12 w-full rounded-10 border bg-white">
-              {searchedTags.map((searchedTag: string) => (
-                <li
-                  key={searchedTag}
-                  className="text-gray-8 flex items-center justify-between border-b border-gray-2 px-5 py-3 last:border-none"
-                >
-                  <span className="b6">#{searchedTag}</span>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      const newTag = searchedTag.trim()
-                      if (!newTag) return
-                      if (selectedTags.includes(newTag)) return
-                      if (selectedTags.length >= 3) {
-                        alert('태그는 최대 3개까지 추가할 수 있습니다.')
-                        return
-                      }
-                      setSelectedTags((prev) => [...prev, newTag])
-                      setSearchTag('')
-                    }}
-                  >
-                    <img src="/icons/add-icon.svg" />
-                  </button>
-                </li>
-              ))}
+              {searchedTags?.map((searchedTag: string) => {
+                return (
+                  <li className="text-gray-8 flex items-center justify-between border-b border-gray-2 px-5 py-3 last:border-none">
+                    <span className="b6">#{searchedTag}</span>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        const newTag = searchedTag.trim()
+
+                        if (!newTag) return
+                        if (selectedTags.includes(newTag)) return
+                        if (selectedTags.length >= 3) {
+                          alert('태그는 최대 3개까지 추가할 수 있습니다.')
+                          return
+                        }
+
+                        setSelectedTags((prev) => [...prev, newTag])
+                        setSearchTag('')
+                      }}
+                    >
+                      <img src="/icons/add-icon.svg" />
+                    </button>
+                  </li>
+                )
+              })}
             </ul>
           )}
         </div>
@@ -188,6 +214,7 @@ export default function MypageEditContainer() {
           />
         </div>
 
+        {/* ✅ 파일 선택 후 파일명 보여주기 */}
         {selectedFile && (
           <p className="b6 mt-2 text-gray-6">{selectedFile.name}</p>
         )}
