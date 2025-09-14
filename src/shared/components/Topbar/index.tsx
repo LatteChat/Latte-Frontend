af273446bcc642bd595716a299a6a304124d8f93
KimYuKyung <kyg0954@naver.com>
2025-09-14 16:17:21 +0900

    feat: topbar 수정

--- src/shared/components/Topbar/index.tsx
+++ src/shared/components/Topbar/index.tsx
@@ -20,32 +20,27 @@ export default function Topbar({ icons }: { icons: TopbarIcons }) {
         <img src="/images/lattechat-logo.svg" />
       </Link>
       <div className="flex gap-3">
-        {!isLoading &&
-          icons.map((icon, index) => {
-            if (!userInfo && index === 0) {
-              return (
-                <Link
-                  key={icon.alt}
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao`}
-                >
-                  <img
-                    src="/icons/login-icon.svg"
-                    alt="로그인"
-                    className="h-6"
-                  />
-                </Link>
-              )
-            }
+        {icons.map((icon, index) => {
+          if (!userInfo && index === 0) {
             return (
-              <Link key={icon.alt} href={icon.href}>
-                <img
-                  src={icon.iconUrl}
-                  alt={icon.alt + '아이콘'}
-                  className="aspect-square h-6 w-6"
-                />
+              <Link
+                key={icon.alt}
+                href={`${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao`}
+              >
+                <img src="/icons/login-icon.svg" alt="로그인" className="h-6" />
               </Link>
             )
-          })}
+          }
+          return (
+            <Link key={icon.alt} href={icon.href}>
+              <img
+                src={icon.iconUrl}
+                alt={icon.alt + '아이콘'}
+                className="aspect-square h-6 w-6"
+              />
+            </Link>
+          )
+        })}
       </div>
     </header>
   )
